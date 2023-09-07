ca7fec64e7a9c24d3f0f30536552fcd32bd967a4
Constanza de la Fuente <kawnee1747@gmail.com>
2023-09-06 21:38:14 -0500

    todo-001: Fixed build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
@@ -22,8 +22,8 @@ const routes: Routes = [];
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule,
-    RouterModule.forRoot(routes)
+    // AppRoutingModule,
+    // RouterModule.forRoot(routes)
   ],
   // providers: [TodoListService],
   providers: [],
